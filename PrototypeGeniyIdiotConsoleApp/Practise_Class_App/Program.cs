a832ee9f3f53be4e33982817fd69214b1c7cf35a
Victor Byliev <ya.vic4592@yandex.ru>
2020-04-03 18:42:56 +0300

    создал новый проект

---
+++ PrototypeGeniyIdiotConsoleApp/Practise_Class_App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practise_Class_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
