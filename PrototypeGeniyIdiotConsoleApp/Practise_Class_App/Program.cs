4e53abc03513018afd09d09c6f3635ac0e6506cc
Victor Byliev <ya.vic4592@yandex.ru>
2020-04-06 16:57:39 +0300

    Добавил прямоугольник, имправил треугольник

--- PrototypeGeniyIdiotConsoleApp/Practise_Class_App/Program.cs
+++ PrototypeGeniyIdiotConsoleApp/Practise_Class_App/Program.cs
@@ -9,16 +9,33 @@ namespace Practise_Class_App
             Point point = new Point(2, 3);
             Console.WriteLine(point.Print());
 
-            Point point1 = new Point(4, 1);
-            Point point2 = new Point(1, 1);
-            Point point3 = new Point(2, 2);
+            Point point1 = new Point(1, 1);
+            Point point2 = new Point(2, 1);
+            Point point3 = new Point(2, 3);
+            Point point4 = new Point(1, 1);
+
             Line line1 = new Line(point1, point2);
             Line line2 = new Line(point2, point3);
             Line line3 = new Line(point3, point1);
+
             Triangle triangle = new Triangle(point1, point1, point3);
+            triangle.Line1 = line1;
+            triangle.Line2 = line2;
+            triangle.Line3 = line3;
+
+            Rectangle rectangle = new Rectangle(point1, point1, point3, point4);
+            Line line4 = new Line(point3, point4);
+            Line line5 = new Line(point4, point1);
+            rectangle.Line1 = line1;
+            rectangle.Line2 = line2;
+            rectangle.Line3 = line4;
+            rectangle.Line4 = line5;
+
             Console.WriteLine(line1.Lenght());
             Console.WriteLine(line1.Print());
-            Console.WriteLine(triangle.Perimetr(line1, line2, line3));
+            Console.WriteLine(triangle.Perimetr());
+            Console.WriteLine(rectangle.Perimetr());
+            Console.WriteLine(rectangle.Square());
         }
     }
 }
--- PrototypeGeniyIdiotConsoleApp/Practise_Class_App/Rectangle.cs
+++ PrototypeGeniyIdiotConsoleApp/Practise_Class_App/Rectangle.cs
@@ -4,31 +4,32 @@ using System.Text;
 
 namespace Practise_Class_App
 {
-    class Triangle
+    class Rectangle
     {
         public Point Point1;
         public Point Point2;
         public Point Point3;
+        public Point Point4;
         public Line Line1;
         public Line Line2;
         public Line Line3;
+        public Line Line4;
 
-        public Triangle(Point point1, Point point2, Point point3)
+        public Rectangle(Point point1, Point point2, Point point3, Point point4)
         {
             Point1 = point1;
             Point2 = point2;
             Point3 = point3;
+            Point4 = point4;
         }
-
         public double Perimetr()
         {
-           return Line1.Lenght() + Line2.Lenght() + Line3.Lenght();
+            return 2 * (Line1.Lenght() + Line2.Lenght());
         }
 
-        public string Print()
+        public double Square()
         {
-            return "(" + Point1.Print() + "," + Point2.Print() + "," + Point3.Print() + ")";
+            return Line1.Lenght() * Line2.Lenght();
         }
-
     }
 }
