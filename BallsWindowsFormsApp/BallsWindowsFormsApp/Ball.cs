312d96de7793a09344556ae5a0fa3f1cd05ef644
Josef Dzeranov <soslan29@gmail.com>
2020-05-11 20:47:02 +0300

    Сделали наследование

--- BallsWindowsFormsApp/BallsWindowsFormsApp/Ball.cs
+++ BallsWindowsFormsApp/BallsWindowsFormsApp/Ball.cs
@@ -1,14 +1,45 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 
 namespace BallsWindowsFormsApp
 {
     class Ball
     {
+        public int X = 100;
+        public int Y = 100;
+        public Brush Brush = Brushes.Green;
+        public MainForm Form;
         public Ball(MainForm form)
         {
-            var graphics = form.CreateGraphics();
-            var rectangle = new Rectangle(100, 100, 50, 50);
-            graphics.FillEllipse(Brushes.Green, rectangle);
+            Form = form;
+        }
+
+        public void Show()
+        {
+            var graphics = Form.CreateGraphics();
+            var rectangle = new Rectangle(X, Y, 50, 50);
+            graphics.FillEllipse(Brush, rectangle);
+        }
+    }
+
+    class RandomPointBall : Ball
+    {
+        public RandomPointBall(MainForm form) : base(form)
+        {
+            var random = new Random();
+            X = random.Next(0, 500);
+            Y = random.Next(0, 500);
+            Brush = Brushes.Red;
+        }
+    }
+
+    class PointBall : Ball
+    {
+        public PointBall(MainForm form, int x, int y) : base(form)
+        {
+            X = x - 50 / 2;
+            Y = y - 50 / 2;
+            Brush = Brushes.Yellow;
         }
     }
 }
--- BallsWindowsFormsApp/BallsWindowsFormsApp/MainForm.cs
+++ BallsWindowsFormsApp/BallsWindowsFormsApp/MainForm.cs
@@ -13,6 +13,19 @@ namespace BallsWindowsFormsApp
         private void button1_Click(object sender, EventArgs e)
         {
             var ball = new Ball(this);
+            ball.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var ball = new RandomPointBall(this);
+            ball.Show();
+        }
+
+        private void MainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            var ball = new PointBall(this, e.X, e.Y);
+            ball.Show();
         }
     }
 }
