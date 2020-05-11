efb9a22584ad1d9745038dff5dfade950b027906
Josef Dzeranov <soslan29@gmail.com>
2020-05-11 20:28:25 +0300

    Вынесли в класс

---
+++ BallsWindowsFormsApp/BallsWindowsFormsApp/Ball.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+namespace BallsWindowsFormsApp
+{
+    class Ball
+    {
+        public Ball(MainForm form)
+        {
+            var graphics = form.CreateGraphics();
+            var rectangle = new Rectangle(100, 100, 50, 50);
+            graphics.FillEllipse(Brushes.Green, rectangle);
+        }
+    }
+}
---
+++ BallsWindowsFormsApp/BallsWindowsFormsApp/MainForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BallsWindowsFormsApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var ball = new Ball(this);
+        }
+    }
+}
